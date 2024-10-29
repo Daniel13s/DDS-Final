93f8ee38af5a2cc49f5a732fedceac4118146f72
Daniel Silva <165176862+Daniel13s@users.noreply.github.com>
2024-10-29 20:04:28 -0300

    Fim de atualizações para prototipos

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-function clicar() {
-    //Criação do menu
-    var nav = document.getElementById("nav")
-    var body = document.getElementById("body")
-    var menu = document.createElement('div')
-    menu.setAttribute('id', 'menu')
-    nav.appendChild(menu)
+function clicar001() {
+    var t001 = document.getElementById("t001")
+    var popUp = document.createElement("p")
+    popUp.setAttribute("id", "popUp1")
+    popUp.innerText = "O item tem alguns arranhões mas está funcionando perfeitamente"
+    t001.appendChild(popUp)
 
-    //Atribuindo conteudo ao menu
-    //DANIEL
-    var daniel = document.createElement('a')
-    daniel.setAttribute('id', 'daniel')
-    daniel.setAttribute('href', 'https://www.instagram.com/danstart_/')
-    daniel.setAttribute('target', '_blank')
-    daniel.setAttribute('rel', 'external')
-    daniel.innerText = "@danstart_"
-    menu.appendChild(daniel)
+    t001.addEventListener('mouseout', excluir)
+    function excluir() {
+        var popUp = document.getElementById("popUp1")
+        popUp.parentNode.removeChild(popUp)
+    }
+}
 
+function clicar002() {
+    var t001 = document.getElementById("t002")
+    var popUp = document.createElement("p")
+    popUp.setAttribute("id", "popUp2")
+    popUp.innerText = "Está sem o cabo, mas é de boa qualidade"
+    t001.appendChild(popUp)
 
-    //Excluindo o menu
-    body.addEventListener('click', excluirpc)
-    function excluirpc() {
-        var menu = document.getElementById('menu')
-        menu.parentNode.removeChild(menu)
+    t001.addEventListener('mouseout', excluir)
+    function excluir() {
+        var popUp = document.getElementById("popUp2")
+        popUp.parentNode.removeChild(popUp)
+    }
+}
+
+function clicar003() {
+    var t001 = document.getElementById("t003")
+    var popUp = document.createElement("p")
+    popUp.setAttribute("id", "popUp3")
+    popUp.innerText = "Usada poucas vezes"
+    t001.appendChild(popUp)
+
+    t001.addEventListener('mouseout', excluir)
+    function excluir() {
+        var popUp = document.getElementById("popUp3")
+        popUp.parentNode.removeChild(popUp)
     }
 }
